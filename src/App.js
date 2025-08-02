dca4df5f93da6cdf31b293a15bde58edc6c24719
Anneke <122925139+anneke44@users.noreply.github.com>
2025-08-02 18:27:35 +0200

    Added logo and header to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
-import bootstrap from "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "./AnnekeLogo.png";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Hallo, this is a dictionary application!
+    <div className="App container">
+      <header className="App-header m-5 d-flex flex-row align-items-center align-self-center justify-content-center">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+        <div className="App-name">Dictionary Application</div>
       </header>
     </div>
   );
