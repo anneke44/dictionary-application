e12ae9d86926a9c9b3b7d24ac8b5e899b139415d
Anneke <122925139+anneke44@users.noreply.github.com>
2025-08-03 17:52:20 +0200

    Update Synonym.js display

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,5 +1,22 @@
 import React from "react";
 
 export default function Synonyms(props) {
-  return "Synonyms";
+  if (props.synonyms) {
+    return (
+      <div className="Synonyms">
+        <strong>Synonyms:</strong>
+        {props.synonyms.map(function (synonym, index) {
+          return (
+            <span key={index}>
+              {" "}
+              {index > 0 ? ", " : " "}
+              {synonym}
+            </span>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
